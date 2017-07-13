142d42556ef427a0c7ae85f9f176da26c8c99d3c
JSpiner <jspiner@naver.com>
2017-07-13 14:06:27 +0900

    init project

---
+++ app/src/main/java/net/jspiner/cachebanksample/MainActivity.java
@@ -0,0 +1,17 @@
+package net.jspiner.cachebanksample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.jspiner.cachebank.Bank;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Bank bank;
+    }
+}
---
+++ cachebank/src/main/java/net/jspiner/cachebank/Bank.java
@@ -0,0 +1,8 @@
+package net.jspiner.cachebank;
+
+/**
+ * Created by jspiner on 2017. 7. 13..
+ */
+
+public class Bank {
+}
